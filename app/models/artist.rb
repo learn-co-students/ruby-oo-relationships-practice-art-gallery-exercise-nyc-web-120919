98dfe2c5b8420ba217d169b7487185795817d2fc
kennja05 <jacobkenny05@gmail.com>
2019-12-14 19:27:35 -0500

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,10 +1,54 @@
 class Artist
 
   attr_reader :name, :years_experience
+  @@all = []
 
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end 
+
+  def paintings
+    Painting.all.select do |painting|
+      painting.artist == self
+    end 
+  end 
+
+  def galleries
+    artist_galleries = []
+    paintings.each do |painting|
+      artist_galleries << painting.gallery 
+    end 
+    artist_galleries.uniq 
+  end
+
+  def cities
+    artist_cities = []
+    galleries.each do |gallery|
+      artist_cities << gallery.city 
+    end 
+    artist_cities.uniq
+  end 
+
+  def self.total_experience
+    running_total = 0
+    self.all.each do |artist|
+      running_total += artist.years_experience
+    end 
+    running_total
+  end
+
+  def self.most_prolific
+    self.all.max_by {|artist| artist.paintings.length / artist.years_experience}
+  end 
+
+  def create_painting(title, price, gallery)
+    Painting.new(title, price, self, gallery)
+  end 
+  
 end
--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,10 +1,55 @@
+require 'pry'
+
 class Gallery
 
   attr_reader :name, :city
+  @@all = []
 
   def initialize(name, city)
     @name = name
     @city = city
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end 
+
+
+  def all_paintings
+    #returns all painting instances for the gallery (which includes extra info)
+    Painting.all.select do |painting|
+      painting.gallery == self
+    end  
+  end 
+
+  def paintings
+    all_paintings.map do |painting|
+      "This painting is called '#{painting.title}', it was painted by #{painting.artist.name}, and it is worth $#{painting.price}."
+    end 
+  end 
+
+  def artists
+    the_artists = []
+    Painting.all.each do |painting|
+      if painting.gallery == self && !the_artists.include?(painting.artist)
+        the_artists << painting.artist
+      end 
+    end 
+    the_artists
+  end 
+
+  def artist_names
+    artists.map do |artist|
+      artist.name
+    end
+  end 
+
+  def most_expensive_painting
+    expensive_painting = self.all_paintings.max_by do |painting|
+      painting.price
+    end 
+    expensive_painting
+  end 
 
 end
